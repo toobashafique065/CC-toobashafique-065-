998dd21494ed9ef343cc28d910cb22d401af0518
Tooba Shafique <23-22411-065@se.fjwu.edu.pk>
2026-01-19 22:38:44 +0500

    Create security.tf

---
+++ Lab_exam/terraform/security.tf
@@ -0,0 +1,35 @@
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [local.my_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
